503e381b81f324a577925cc65e58a6ed7e0c59fb
Serhii <kravchenkoss1541@gmail.com>
2024-12-01 21:52:10 +0200

    add solution for topic_02 and topic_03

--- topic_02/task1.py
+++ topic_02/task1.py
@@ -1,5 +1,9 @@
-def solve_kvadr_rivn(a, b, c):
-    d = b ** 2 - 4 * a * c
+
+def find_discriminator(a, b, c):
+    return b ** 2 - 4 * a * c
+
+def find_roots(a, b, c):
+    d = find_discriminator(a, b, c)
     if d < 0:
         return None
     x1 = (-b + d ** 0.5) / (2 * a)
@@ -13,6 +17,13 @@ c_str = input("Уведіть c: ")
 
 answer = solve_kvadr_rivn(int(a_str), int(b_str), int(c_str))
 
+<<<<<<< HEAD
+=======
+coeffs = input_call()
+answer = find_roots(coeffs[0], coeffs[1], coeffs[2])
+
+print()
+>>>>>>> 77b9501 (add solution for topic_02 and topic_03)
 if answer is None:
     print('Коренів не знайдено оскільки Дискримінант від\'ємний.')
 else:
--- topic_02/task2.py
+++ topic_02/task2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def calculator_if(a, b, action):
     if action == '+':
         return a + b
@@ -27,3 +28,59 @@ action_str = input("Уведіть дію(+, -, *, /): ")
 
 print(f'\nОтримане число за допомогою if калькулятора: ', calculator_if(int(a_str), int(b_str), action_str))
 print(f'\nОтримане число за допомогою match калькулятора: ', calculator_match(int(a_str), int(b_str), action_str))
+=======
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return 'Ділення на нуль заборонено.'
+    return a / b
+
+
+def power(a, b):
+    return a ** b
+
+
+def calculator(a, b, action):
+    if action == '1':
+        return add(a, b)
+    elif action == '2':
+        return subtract(a, b)
+    elif action == '3':
+        return multiply(a, b)
+    elif action == '4':
+        return divide(a, b)
+    elif action == '5':
+        return power(a, b)
+
+
+def input_call():
+    print('''Список можливих дій:
+    1. Додавання
+    2. Віднімання
+    3. Множення
+    4. Ділення
+    5. Піднесення до степеня''')
+    action = input("Оберіть дію: ")
+    if action in ('1', '2', '3', '4', '5'):
+        a = float(input("Уведіть число a: "))
+        b = float(input("Уведіть число b: "))
+        return a, b, action
+    else:
+        print('Обрано некоректну дію.')
+
+
+input_data = input_call()
+if input_data is not None:
+    print(f'\nРезультат: {calculator(input_data[0], input_data[1], input_data[2])}')
+>>>>>>> 77b9501 (add solution for topic_02 and topic_03)
